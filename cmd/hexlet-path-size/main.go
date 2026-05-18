387a203260b979eb79141aae323e5515c980cfe0
Vyacheslav Korolev <slavakorolev2@gmail.com>
2026-05-18 16:44:37 +0400

    Initialize Go project with basic structure, including .gitignore, configuration files, and core functionality for calculating file and directory sizes. Add tests for path size functionality and example data files.

---
+++ cmd/hexlet-path-size/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"github.com/urfave/cli/v3"
+	"context"
+	"os"
+	"log"
+	"code"
+)
+
+func main() {
+	cmd := &cli.Command{
+		Name:  "hexlet-path-size",
+        Usage: "print size of a file or directory",
+        Action: func(ctx context.Context, cmd *cli.Command) error {
+			path := cmd.Args().Get(0)
+			if path == "" {
+				return fmt.Errorf("argument is required")
+			}
+            fileSize, error := code.GetPathSize(path, cmd.Bool("recursive"), cmd.Bool("human"), cmd.Bool("all"))
+            if error != nil {
+                return  error
+            }
+            result := fmt.Sprintf("%s\t%s", fileSize, path)
+            fmt.Println(result)
+            return nil
+        },
+        Flags: []cli.Flag{
+            &cli.BoolFlag{
+                Name:  "recursive",
+                Aliases: []string{"r"},
+                Usage: "include hidden files and directories",
+                DefaultText: "false",
+            },
+            &cli.BoolFlag{
+                Name:  "human",
+                Aliases: []string{"H"},
+                Usage: "human-readable sizes (auto-select unit)",
+                DefaultText: "false",
+            },
+            &cli.BoolFlag{
+                Name:  "all",
+                Aliases: []string{"a"},
+                Usage: "include hidden files and directories",
+                DefaultText: "false",
+            },
+        },
+    }
+
+    if err := cmd.Run(context.Background(), os.Args); err != nil {
+        log.Fatal(err)
+    }
+}
---
+++ tests/path_size_test.go
@@ -0,0 +1,37 @@
+package code_test
+
+import (
+	"testing"
+	"github.com/stretchr/testify/require"
+	"code"
+)
+
+func TestGetPathSize(t *testing.T) {
+	testCases := [] struct {
+		name string
+		path string
+		recursive bool
+		human bool
+		all bool
+		expected string
+	}{
+		{name: "file", path: "./../testdata/file1.txt", recursive: false, human: false, all: false, expected: "2906B"},
+		{name: "directory", path: "./../testdata/in", recursive: false, human: false, all: false, expected: "2412B"},
+		{name: "directory human", path: "./../testdata/in", recursive: false, human: true, all: false, expected: "2.4KB"},
+		{name: "directory hidden", path: "./../testdata/in", recursive: false, human: false, all: true, expected: "2512B"},
+		{name: "empty directory", path: "./../testdata/empty", recursive: false, human: false, all: false, expected: "0B"},
+		{name: "recursive directory", path: "./../testdata", recursive: true, human: false, all: false, expected: "5318B"},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			result, _ := code.GetPathSize(testCase.path, testCase.recursive, testCase.human, testCase.all)
+			require.Equal(t, testCase.expected, result)
+		})
+	}
+
+	t.Run("non-existent path", func(t *testing.T) {
+		_, error := code.GetPathSize("./../testdata/adjfgjkdasf", false, false, false)
+		require.Error(t, error)
+	})
+}
