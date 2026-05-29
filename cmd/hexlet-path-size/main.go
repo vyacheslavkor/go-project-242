6231acd5c13864c2fa013d8aad985f02b445a92e
Vyacheslav Korolev <slavakorolev2@gmail.com>
2026-05-29 08:38:01 +0400

    Validate command argument count in hexlet-path-size and update usage instructions for clarity.

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -14,14 +14,16 @@ func main() {
 		Name:  "hexlet-path-size",
         Usage: "print size of a file or directory",
         Action: func(ctx context.Context, cmd *cli.Command) error {
+            if cmd.Args().Len() != 1 {
+                return cli.Exit(fmt.Sprintf("incorrect usage: expected 1 argument, got %d", cmd.Args().Len()), 1)
+            }
+
 			path := cmd.Args().Get(0)
-			if path == "" {
-				return fmt.Errorf("argument is required")
-			}
             fileSize, error := code.GetPathSize(path, cmd.Bool("recursive"), cmd.Bool("human"), cmd.Bool("all"))
             if error != nil {
                 return  error
             }
+
             result := fmt.Sprintf("%s\t%s", fileSize, path)
             fmt.Println(result)
             return nil
@@ -46,6 +48,7 @@ func main() {
                 DefaultText: "false",
             },
         },
+        ArgsUsage: "<path>",
     }
 
     if err := cmd.Run(context.Background(), os.Args); err != nil {
